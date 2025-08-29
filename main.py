8acb90b07cbf28d96dba782c7076dc7278ec479f
Chadrick <chadrickclarkea@gmail.com>
2025-08-29 09:36:12 -0400

    "updates- debugging cloudflare block"

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from src.fpl_api_scraper import get_fpl_metadata,get_fpl_metadata_cached,get_manager_team,create_player_map
 from src.fpl_api_scraper import get_fixtures, create_team_mapping, get_fixture_difficulty
+from src.prem_inj_scraper import get_player_injury_status
 import httpx
 #import json
 #import time
@@ -18,8 +19,7 @@ async def get_manager_team_details(team_id:int,game_week:int):
     bootstrap_data = await get_fpl_metadata_cached()
     player_map = await create_player_map(bootstrap_data)
     fixtures_metadata = await get_fixtures()
-    team_mapping = await create_team_mapping(bootstrap_data)
-
+    injury_map = await get_player_injury_status()
     
     if not manager_team_picks or not bootstrap_data or not fixtures_metadata:
         print("One or more essential data sources could not be retrieved from API")
@@ -38,9 +38,11 @@ async def get_manager_team_details(team_id:int,game_week:int):
                 player_map=player_map,
                 fixtures_metadata=fixtures_metadata
                 )
-            print(f"next 3 matches: {fixture_difficulty}")
-            print("fixture difficulty datatype:", type(fixture_difficulty))
-            combined_data = pick | player_details | {"future_fixtures":fixture_difficulty}
+            
+            player_name = player_details.get("web_name")
+            fitness_status = injury_map.get(player_name,"Fit")
+
+            combined_data = pick | player_details | {"future_fixtures":fixture_difficulty} | {"fitness_status": fitness_status}
             manager_team_details.append(combined_data)
 
     print(f"Successfully retrieved details for manager's team of {len(manager_team_details)} players.")

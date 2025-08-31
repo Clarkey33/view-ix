1ae2d6567588f04623bb4c9894216e7757242956
Chadrick <chadrickclarkea@gmail.com>
2025-08-31 04:37:41 -0400

    "updates- refactor to config-debugging Render issues"

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-from dotenv import dotenv_values
-env_tokens = dotenv_values('.env')
+#from dotenv import dotenv_values
+from dotenv import load_dotenv
+import os
+
+#env_tokens = dotenv_values('.env')
 
 
 
-CACHE_DURATION_SECONDS = int(env_tokens.get('CACHE_DURATION_SECONDS'))
-LAST_CACHE_TIME = float(env_tokens.get('LAST_CACHE_TIME'))
-BASE_URL = env_tokens.get('BASE_URL')
+CACHE_DURATION_SECONDS = int(os.getenv('CACHE_DURATION_SECONDS'))
+LAST_CACHE_TIME = float(os.getenv('LAST_CACHE_TIME'))
+BASE_URL = os.getenv('BASE_URL')
 #BASE_URL_INJ = env_tokens.get('BASE_URL_INJ')
 

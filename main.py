977541ecd05fca775cd5c78c00f6809a23b0fb94
Chadrick <chadrickclarkea@gmail.com>
2025-08-31 05:11:28 -0400

    "resolving CORS policy issue on frontend"

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["*"]
+#origins = ["*"]
+origins = [
+    "https://view-ix.vercel.app/",
+    "http://127.0.0.1:8001",
+    "http://localhost:8001",
+]
 
 app.add_middleware(
     CORSMiddleware,
